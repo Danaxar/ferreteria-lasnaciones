ea12ff50e78087bb0c2565292ac44090c4cf3590
Daniel <daniel.catalan@usach.cl>
2023-08-05 12:45:37 -0400

    feat: creación de tablas y datos

---
+++ database/create.sql
@@ -0,0 +1,27 @@
+create table departamento(
+    id_departamento SERIAL PRIMARY KEY,
+    nombre_departamento varchar(25)
+);
+
+create table tipoventa(
+    id_tipoventa SERIAL PRIMARY KEY,
+    nombre_tipoventa varchar(25)
+);
+
+create table proveedor(
+    id_proveedor SERIAL PRIMARY KEY,
+    nombre_proveedor varchar(50)
+);
+
+drop table inventario;
+
+create table inventario(
+    codigo SERIAL PRIMARY KEY,
+    codigo_producto varchar(20),
+    producto varchar(100),
+    pventa int,
+    pmayoreo int,
+    id_departamento int REFERENCES departamento(id_departamento),
+    existencia float,
+    id_proveedor int REFERENCES proveedor(id_proveedor)
+);
