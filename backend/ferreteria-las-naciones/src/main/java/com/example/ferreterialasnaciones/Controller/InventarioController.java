9b32b5b110f13dd93183419aa4a61d1c64bbe0b6
Daniel <daniel.catalan@usach.cl>
2023-08-16 17:41:35 -0400

    feat: inventario controller

--- backend/ferreteria-las-naciones/src/main/java/com/example/ferreterialasnaciones/Controller/InventarioController.java
+++ backend/ferreteria-las-naciones/src/main/java/com/example/ferreterialasnaciones/Controller/InventarioController.java
@@ -3,6 +3,7 @@ package com.example.ferreterialasnaciones.Controller;
 import com.example.ferreterialasnaciones.Entities.Inventario;
 import com.example.ferreterialasnaciones.Service.InventarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventarios")
+@CrossOrigin("*")
 public class InventarioController {
     @Autowired
     InventarioService inventarioService;
--- backend/ferreteria-las-naciones/src/main/java/com/example/ferreterialasnaciones/Entities/Inventario.java
+++ backend/ferreteria-las-naciones/src/main/java/com/example/ferreterialasnaciones/Entities/Inventario.java
@@ -2,23 +2,37 @@ package com.example.ferreterialasnaciones.Entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class Inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column(name = "id_inventario")
     private Integer idInventario;
+
+    @Column(name = "codigo_producto")
     private String codigoProducto;
+
+    @Column(name = "producto")
     private String producto;
+
+    @Column(name = "pventa")
     private Integer pventa;
+
+    @Column(name = "pmayoreo")
     private Integer pmayoreo;
+
+    @Column(name = "id_departamento")
     private Integer idDepartamento;
+
+    @Column(name = "existencia")
     private Float existencia;
+
+    @Column(name = "tipoventa")
     private Integer tipoventa;
+
+    @Column(name = "id_proveedor")
     private Integer idProveedor;
 }
